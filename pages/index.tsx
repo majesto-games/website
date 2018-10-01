c13454bdc05652c969dbdc434ffe93e58d1bd714
Niklas <me@sodapop.se>
2018-10-01 15:20:31 +0200

    2x logo and animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import logo from "../images/dark.png"
+import logo2x from "../images/dark@2x.png"
 import "./index.css"
 import * as React from "react"
 
 export default () => (
   <div className="Index">
-    <img src={logo} />
+    <img src={logo} srcSet={`${logo} 1x, ${logo2x} 2x`} />
   </div>
 )
